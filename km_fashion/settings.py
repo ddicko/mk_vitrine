da2ba71c346bf61821d31419f96259314effb731
ddicko <dicko.dev@gmail.com>
2025-10-01 10:00:01 +0000

    release 0.0.1

--- km_fashion/settings.py
+++ km_fashion/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.172.28.10"]
+ALLOWED_HOSTS = ["127.0.0.1","10.172.28.10"]
 
 
 # Application definition
--- km_fashion/urls.py
+++ km_fashion/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("gonga_admin_tiekoumaba/", admin.site.urls),
     path("", include("store.urls")),
 ]
 
