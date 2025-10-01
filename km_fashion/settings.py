2107420b58eb80fa2d5a15597deda40fe39cbaec
ddicko <dicko.dev@gmail.com>
2025-10-01 08:59:19 +0000

    allowed host set

--- km_fashion/settings.py
+++ km_fashion/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["10.172.28.10"]
 
 
 # Application definition
